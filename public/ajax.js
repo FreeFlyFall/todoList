38b3051de7e25ec003f54ef87842e571fce568dd
Ryan VanSlyke <ryanjvanslyke@gmail.com>
2018-07-17 08:49:37 -0400

    added search function

--- public/ajax.js
+++ public/ajax.js
@@ -75,18 +75,52 @@ $('#todo-list').on('submit', '.edit-item-form', function (e) {
 $('#todo-list').on('submit', '.delete-item-form', function (e) {
     e.preventDefault();
     var confirmResponse = confirm('Are you sure you want to delete this item?');
-    if(confirmResponse){
+    if (confirmResponse) {
         var actionUrl = $(this).attr('action');
         $itemToDelete = $(this).closest('.list-group-item');
         $.ajax({
             url: actionUrl,
             type: 'DELETE',
             itemToDelete: $itemToDelete,
-            success: function(data) {
+            success: function (data) {
                 this.itemToDelete.remove();
             }
         })
     } else {
         $(this).find('button').blur();
     }
+});
+
+// Search functionality
+
+$('#search').on('input', function (e) {
+    e.preventDefault();
+    $.get(`/todos?keyword=${encodeURIComponent(e.target.value)}`, function (data) {
+        $('#todo-list').html('');
+        data.forEach(function (todo) {
+            $('#todo-list').append(
+                `
+				<li class="list-group-item">
+					<form action="/todos/${todo._id}" method="POST" class="edit-item-form">
+						<div class="form-group">
+							<label for="${todo._id}">Item Text</label>
+							<input type="text" value="${todo.text}" name="todo[text]" class="form-control" id="${todo._id}">
+						</div>
+						<button class="btn btn-primary">Update Item</button>
+					</form>
+					<span class="lead">
+						${todo.text}
+					</span>
+					<div class="pull-right">
+						<button class="btn btn-sm btn-warning edit-button">Edit</button>
+						<form style="display: inline" method="POST" action="/todos/${todo._id}" class="delete-item-form">
+							<button type="submit" class="btn btn-sm btn-danger">Delete</button>
+						</form>
+					</div>
+					<div class="clearfix"></div>
+				</li>
+				`
+            );
+        });
+    });
 });
